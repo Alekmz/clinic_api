c182ae7c1d3bde631662fcba36076b6823e864b4
Alekmz <amalexmuniz@gmail.com>
2025-08-12 20:03:46 -0300

    feat: concept review

--- src/app.js
+++ src/app.js
@@ -5,14 +5,13 @@ const app = express()
 app.use(express.json())
 
 app.get('/usuarios', async (request, response) => {
-try{
-    const usuarios = await prismaClient.usuario.findMany();
-    return response.json(usuarios)
-}
-catch (e){
-        console.log(e)
-}
-
-  });
+    try{
+        const usuarios = await prismaClient.usuario.findMany();
+        return response.json(usuarios)
+    }
+    catch (e){
+            console.log(e)
+    }
+});
 
 app.listen(3000, ()=> console.log("Api rodando"))
